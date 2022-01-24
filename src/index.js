abcc2c764ae5eb39dcde500f0841e95f700f6a65
MartinSugasti <martinsugasti@gmail.com>
2022-01-24 11:12:56 +0100

    Connect with openweathermap and retrive useful data.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.scss';
 import '@fortawesome/fontawesome-free/js/all';
+import requestsController from './modules/requests_controller';
+
+requestsController.getWeatherByLocation('Barcelona');
+requestsController.getWeatherByLocation('qwerty6543');
+requestsController.getWeatherByLocation('London');
