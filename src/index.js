8059fb17ca383d54c3635d706f908dd91cc6b750
Martin Sugasti <56696387+MartinSugasti@users.noreply.github.com>
2022-01-26 10:19:43 +0100

    Add real initial forecast. (#6)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './styles/style.scss';
 import '@fortawesome/fontawesome-free/js/all';
 import displayController from './modules/display_controller';
 
+displayController.showInitialForecast();
 displayController.addSearchFunctionality();
 displayController.addUnitFunctionality();
